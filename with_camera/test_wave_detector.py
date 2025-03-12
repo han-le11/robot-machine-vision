74d6afe9e1165e2506f8647fa0e29ece10fe357e
han.le@aalto.fi <le.nm.han@gmail.com>
2025-03-12 09:57:06 +0200

    script to run WaveDetector and work with the intel camera

---
+++ with_camera/test_wave_detector.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+current_dir = os.path.dirname(__file__)
+project_dir = os.path.dirname(current_dir)
+
+sys.path.insert(0, project_dir)
+
+from models.wave_detector import WaveDetector
+
+if __name__ == "__main__":
+    detector = WaveDetector()
+    detector.process_camera()
